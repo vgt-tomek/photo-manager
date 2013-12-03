19dcf882ad742058c54de6414dcad5b452373c46
VGT <tomek@vgtworld.pl>
2013-12-03 18:59:41 +0000

    Changed map with dates to TreeMap so it will be sorted.

--- src/main/java/pl/vgtworld/photomanager/filters/DateFilter.java
+++ src/main/java/pl/vgtworld/photomanager/filters/DateFilter.java
@@ -1,16 +1,16 @@
 package pl.vgtworld.photomanager.filters;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import pl.vgtworld.photomanager.model.PhotoContainer;
 
 public class DateFilter {
 	
 	public Map<String, List<PhotoContainer>> groupPhotosByDay(List<PhotoContainer> photos) {
-		Map<String, List<PhotoContainer>> photosGroupedByDay = new HashMap<>();
+		Map<String, List<PhotoContainer>> photosGroupedByDay = new TreeMap<>();
 		for (PhotoContainer photo : photos) {
 			long timestamp = photo.getLastModified();
 			String date = String.format("%1$tY-%1$tm-%1$td", timestamp);
