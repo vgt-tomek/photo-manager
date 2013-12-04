738647e4dcfdae64b74922e05cad4e7123238574
VGT <tomek@vgtworld.pl>
2013-12-04 22:12:00 +0000

    Extracted messages into constants.

--- src/main/java/pl/vgtworld/photomanager/PhotoManager.java
+++ src/main/java/pl/vgtworld/photomanager/PhotoManager.java
@@ -8,6 +8,20 @@ import pl.vgtworld.photomanager.cli.Cli;
 
 public class PhotoManager {
 	
+	private static final String ARGUMENT_VALUE_DIGITS = "d";
+
+	private static final String ARGUMENT_VALUE_PATH = "p";
+
+	private static final String ARGUMENT_VALUE_HELP = "h";
+
+	private static final String INVALID_NUMBER_FOR_D_PARAMETER_MESSAGE = "Invalid number provided for -d parameter.";
+
+	private static final String MISSING_NUMBER_OF_DIGITS_PARAMETER_MESSAGE = "Number of digits for renaming files is required.";
+
+	private static final String MISSING_DIRECTORY_MESSAGE = "Path to target directory is required.";
+	
+	private static final String SEE_HELP_MESSAGE = " Use -h for more information.";
+	
 	private static final Logger LOGGER = Logger.getLogger(PhotoManager.class);
 	
 	public static void main(String[] args) {
@@ -15,7 +29,7 @@ public class PhotoManager {
 			Cli cli = new Cli();
 			cli.parseArgs(args);
 			
-			if (cli.hasOption("h") || cli.getAction() == Cli.Action.NONE) {
+			if (cli.hasOption(ARGUMENT_VALUE_HELP) || cli.getAction() == Cli.Action.NONE) {
 				cli.printHelp();
 				return;
 			}
@@ -33,35 +47,35 @@ public class PhotoManager {
 	}
 	
 	private static void deleteAction(Cli cli) {
-		if (!cli.hasOption("p")) {
-			LOGGER.error("Path to target directory is required. Use -h for more information.");
+		if (!cli.hasOption(ARGUMENT_VALUE_PATH)) {
+			LOGGER.error(MISSING_DIRECTORY_MESSAGE + SEE_HELP_MESSAGE);
 			return;
 		}
 		
-		String path = cli.getOptionValue("p");
+		String path = cli.getOptionValue(ARGUMENT_VALUE_PATH);
 		Launcher launcher = new Launcher();
 		launcher.delete(path);
 	}
 	
 	private static void renameAction(Cli cli) {
-		if (!cli.hasOption("p")) {
-			LOGGER.error("Path to target directory is required. Use -h for more information.");
+		if (!cli.hasOption(ARGUMENT_VALUE_PATH)) {
+			LOGGER.error(MISSING_DIRECTORY_MESSAGE + SEE_HELP_MESSAGE);
 			return;
 		}
-		if (!cli.hasOption("d")) {
-			LOGGER.error("Number of digits for renaming files is required.");
+		if (!cli.hasOption(ARGUMENT_VALUE_DIGITS)) {
+			LOGGER.error(MISSING_NUMBER_OF_DIGITS_PARAMETER_MESSAGE + SEE_HELP_MESSAGE);
 			return;
 		}
 		
-		String path = cli.getOptionValue("p");
+		String path = cli.getOptionValue(ARGUMENT_VALUE_PATH);
 		int digits = 0;
 		try {
-			digits = Integer.parseInt(cli.getOptionValue("d"));
+			digits = Integer.parseInt(cli.getOptionValue(ARGUMENT_VALUE_DIGITS));
 			if (digits < 1) {
 				throw new NumberFormatException("Value below one");
 			}
 		} catch (NumberFormatException e) {
-			LOGGER.error("Invalid number provided for -d parameter.");
+			LOGGER.error(INVALID_NUMBER_FOR_D_PARAMETER_MESSAGE + SEE_HELP_MESSAGE);
 			return;
 		}
 		
